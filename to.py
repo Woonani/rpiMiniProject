034b431e15a6d20e8e846d3089725efb8fcfa32d
Woonani <uranos3377@gmail.com>
2022-10-29 11:25:29 +0100

    되라

---
+++ to.py
@@ -0,0 +1,88 @@
+#print('haha')
+#while True:
+#    print('WTL')
+
+###################################################################
+
+# import RPi.GPIO as GPIO
+# 
+# button_pin =27
+# 
+# 
+# 
+# GPIO.setmode(GPIO.BCM)
+# 
+# GPIO.setup(button_pin, GPIO.IN)
+# 
+# try:
+#     while True:
+#         buttonInput = GPIO.input(button_pin)
+#         print(buttonInput)
+#         
+# except KeyboardInterrupt:
+#     pass
+# 
+# GPIO.cleanup()
+
+####################################################################
+
+# import RPi.GPIO as GPIO
+# 
+# button_pin =27
+# led_pin =22
+# 
+# 
+# GPIO.setmode(GPIO.BCM)
+# 
+# GPIO.setup(button_pin, GPIO.IN)
+# GPIO.setup(led_pin, GPIO.OUT)
+# 
+# #GPIO.output(led_pin, True)
+# 
+# try:
+#     while True:
+#         buttonInput = GPIO.input(button_pin)
+#         GPIO.output(led_pin, buttonInput)
+#         
+# except KeyboardInterrupt:
+#     pass
+# 
+# GPIO.cleanup()
+####################################################################
+# 
+import RPi.GPIO as GPIO
+
+button_pin =27
+led_pin =22
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(button_pin, GPIO.IN)
+GPIO.setup(led_pin, GPIO.OUT)
+
+buttonInputPrev = False
+ledOn = False
+
+try:
+    while True:
+        buttonInput = GPIO.input(button_pin)
+        
+        if buttonInput and not buttonInputPrev:
+            print("check",buttonInput)
+            print("rising edge")
+            ledOn = True if not ledOn else False
+            GPIO.output(led_pin, ledOn)
+#             GPIO.output(led_pin, buttonInput)
+        elif not buttonInput and buttonInputPrev:
+            print("check2",buttonInput)
+            print("falling edge")
+        else: pass
+        
+        buttonInputPrev = buttonInput
+        
+        
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
